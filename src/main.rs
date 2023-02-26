3f6526eb8ae76e58bef62518ad082a144c092cba
siddhant <siddhantkumarupmanyu@gmail.com>
2023-02-26 20:47:33 +0530

    spike e2e test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+#![allow(non_snake_case)]
+
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::io::Result;
+
 mod spike;
 
-fn main() {
-    println!("Hello, world!");
+pub fn main() -> Result<()> {
+    let mut file = File::open("./resources/test_puzzle.csv")?;
+    let mut bufferedReader = BufReader::new(file);
+    let mut contents = String::new();
+
+    bufferedReader.read_to_string(&mut contents)?;
+
+    println!("{contents}");
+
+    spike::readFile();
+
+    Ok(())
 }
