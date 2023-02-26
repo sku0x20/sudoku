d48a4180744b5322983d723b497cae31b6f625bd
siddhant <siddhantkumarupmanyu@gmail.com>
2023-02-26 22:05:14 +0530

    remove unnecessary code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 #![allow(non_snake_case)]
 
-use std::fs::File;
-use std::io::{BufReader, BufWriter, Read, Write};
+use std::fs;
 use std::io::Result;
 
 #[allow(dead_code)]
@@ -10,20 +9,7 @@ fn main() -> Result<()> {
 }
 
 pub fn runApp() -> Result<()> {
-    let file = File::open("./resources/test_puzzle.csv")?;
-    let mut bufferedReader = BufReader::new(file);
-    let mut contents = String::new();
-
-    bufferedReader.read_to_string(&mut contents)?;
-
-    println!("{contents}");
-
-    let file = File::create("./resources/test_puzzle_solved.csv")?;
-    let mut bufferedWriter = BufWriter::new(file);
-
-    bufferedWriter.write_all(b"Test")?;
-
-    bufferedWriter.flush()?;
+    fs::write("./resources/test_puzzle_solved.csv", "test")?;
 
     Ok(())
 }
