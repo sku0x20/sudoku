e3d467b513f84b012c132ada440af541775d46bf
siddhant <siddhantkumarupmanyu@gmail.com>
2023-03-07 19:08:39 +0530

    rename to TestFixture.rs

--- tests/TestFixture.rs
+++ tests/TestFixture.rs
@@ -2,27 +2,6 @@
 
 use std::panic::catch_unwind;
 
-// support for test fixture
-// https://stackoverflow.com/a/38254435
-struct TestFixture {}
-
-impl TestFixture {}
-
-impl Drop for TestFixture {
-    fn drop(&mut self) {}
-}
-
-// todo: can i type def this
-pub fn withFixture(setUp: fn(), tearDown: fn(), testCode: fn()) {
-    withTestFixture(setUp, tearDown, testCode)
-}
-
-fn withTestFixture(setup: fn(), teardown: fn(), testCode: fn()) {
-    setup();
-    testCode();
-    teardown();
-}
-
 static mut setupCalled: bool = false;
 static mut testCodeCalled: bool = false;
 static mut teardownCalled: bool = false;
@@ -70,6 +49,27 @@ fn withPanic() {
     unsafe {
         assert!(teardownCalled);
     }
+}
+
+// support for test fixture
+// https://stackoverflow.com/a/38254435
+struct TestFixture {}
+
+impl TestFixture {}
+
+impl Drop for TestFixture {
+    fn drop(&mut self) {}
+}
+
+// todo: can i type def this
+pub fn withFixture(setUp: fn(), tearDown: fn(), testCode: fn()) {
+    withTestFixture(setUp, tearDown, testCode)
+}
+
+fn withTestFixture(setup: fn(), teardown: fn(), testCode: fn()) {
+    setup();
+    testCode();
+    teardown();
 }
 
 // Would be better if it's like TestFixture::scoped,
