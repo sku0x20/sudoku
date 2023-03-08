a1c2f98dc2d09e2d2b17245e54b1e882adc4c5f5
siddhant <siddhantkumarupmanyu@gmail.com>
2023-03-09 01:20:33 +0530

    test fixture passing

--- tests/TestFixture.rs
+++ tests/TestFixture.rs
@@ -39,38 +39,53 @@ fn withoutPanic() {
     }
 }
 
-// #[test]
-// fn withPanic() {
-//     let _guard = lock.lock().unwrap();
-//
-//     let result = catch_unwind(|| {
-//         withFixture(setup, teardown, || {
-//             unsafe {
-//                 assert!(setupCalled);
-//                 setupCalled = false;
-//                 testCodeCalled = false;
-//                 teardownCalled = false;
-//
-//                 testCodeCalled = true;
-//             }
-//             panic!("should panic");
-//         });
-//     });
-//     assert!(result.is_err());
-//
-//     unsafe {
-//         assert!(teardownCalled);
-//     }
-// }
+#[test]
+fn withPanic() {
+    let _guard = lock.lock().unwrap();
+
+    let result = catch_unwind(|| {
+        withFixture(setup, teardown, || {
+            unsafe {
+                functionsCalled.push(String::from("testCode"));
+            }
+            panic!("should panic");
+        });
+    });
+    assert!(result.is_err());
+
+    unsafe {
+        assert_eq!(functionsCalled[0], "setup");
+        assert_eq!(functionsCalled[1], "testCode");
+        assert_eq!(functionsCalled[2], "teardown");
+    }
+}
 
 // support for test fixture
 // https://stackoverflow.com/a/38254435
-struct TestFixture {}
+struct TestFixture {
+    setup: fn(),
+    teardown: fn(),
+    block: fn(),
+}
 
-impl TestFixture {}
+impl TestFixture {
+    fn new(setup: fn(), teardown: fn(), block: fn()) -> TestFixture {
+        TestFixture {
+            setup,
+            teardown,
+            block,
+        }
+    }
+
+    fn run(&self) {
+        (self.setup)();
+        (self.block)();
+    }
+}
 
 impl Drop for TestFixture {
     fn drop(&mut self) {
+        (self.teardown)();
     }
 }
 
@@ -86,10 +101,10 @@ pub fn scoped(setup: fn(), teardown: fn(), testCode: fn()) {
     scopedImpl(setup, teardown, testCode)
 }
 
+#[inline]
 fn scopedImpl(setup: fn(), teardown: fn(), testCode: fn()) {
-    setup();
-    testCode();
-    teardown();
+    let testFixture = TestFixture::new(setup, teardown, testCode);
+    testFixture.run();
 }
 
 // is drop called for static also.
