1b79acfd09a939e75c55a9be11ce2bfdf4af5380
siddhant <siddhantkumarupmanyu@gmail.com>
2023-03-16 01:04:47 +0530

    FileParser parsesCsvFile

--- src/FileParser.rs
+++ src/FileParser.rs
@@ -1,5 +1,14 @@
-use std::path::Path;
+use std::fs;
 
-pub fn parse(filePath: &str){
-    panic!("test")
+pub fn parse(filePath: &str) -> Vec<u8> {
+    let mut puzzle: Vec<u8> = Vec::with_capacity(81);
+    let contents = fs::read_to_string(filePath).expect("unable to read file");
+    let mut lines = contents.lines();
+    while let Some(line) = lines.next() {
+        let mut spit = line.split(',');
+        while let Some(value) = spit.next() {
+            puzzle.push(value.trim().parse::<u8>().unwrap());
+        }
+    };
+    return puzzle;
 }
--- tests/FileParserTest.rs
+++ tests/FileParserTest.rs
@@ -1,11 +1,12 @@
 #![allow(non_snake_case)]
 
 use std::fs;
+use std::fs::File;
 use std::io::Result;
 use std::panic::catch_unwind;
 use std::path::Path;
 
-use crate::TestFixture::withTestFixture;
+use crate::TestFixture::{withFixture, withTestFixture};
 
 mod TestFixture;
 
@@ -17,17 +18,50 @@ mod FileParser;
 const TEMP_FILE_PATH: &str = "./resources/temp_file.csv";
 
 #[test]
-#[should_panic]
-fn parsePanics() {
-    withTestFixture(setUp, tearDown, || {
-        FileParser::parse(TEMP_FILE_PATH);
+fn parsesCsvFile() {
+    let sampleData = Vec::from(SAMPLE_DATA);
+    let parsedVec = FileParser::parse("./resources/test_puzzle.csv");
+    assert_eq!(parsedVec, sampleData)
+}
+
+#[test]
+#[ignore = "todo: "]
+fn invalidCharacters() {
+    // only 0-9 are valid; 0 representing empty
+    withFixture(setUp, tearDown, || {
+        let result = catch_unwind(|| {
+            writeToFile("1,2,3,4,0,a");
+            FileParser::parse(TEMP_FILE_PATH);
+        });
+        assert!(result.is_err());
+        let result = catch_unwind(|| {
+            writeToFile("1,2,3,4,0,-1");
+            FileParser::parse(TEMP_FILE_PATH);
+        });
+        assert!(result.is_err());
     })
 }
 
 fn setUp() {
-    fs::write(TEMP_FILE_PATH, "test").expect("unable to write to temp file")
+    writeToFile("");
 }
 
 fn tearDown() {
-    fs::remove_file(TEMP_FILE_PATH).expect("temp file should be removed")
+    fs::remove_file(TEMP_FILE_PATH).expect("temp file should be removed");
 }
+
+fn writeToFile(data: &str) {
+    fs::write(TEMP_FILE_PATH, data).expect("unable to write to temp file");
+}
+
+const SAMPLE_DATA: [u8; 81] = [
+    0, 0, 8, 0, 6, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 7, 0, 3, 0,
+    6, 1, 0, 5, 0, 0, 2, 0, 0,
+    4, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 5, 0, 0, 0, 6,
+    2, 9, 0, 6, 0, 0, 1, 0, 0,
+    9, 8, 0, 0, 3, 0, 0, 2, 0,
+    0, 0, 4, 9, 0, 0, 0, 0, 0,
+    0, 0, 5, 0, 0, 0, 8, 0, 0
+];
