73162bb58a92bce81594db86687be90a572837bc
siddhant <siddhantkumarupmanyu@gmail.com>
2023-03-16 01:50:45 +0530

    decouple reading file from parsing it in parse fn

--- src/FileParser.rs
+++ src/FileParser.rs
@@ -1,23 +1,35 @@
 use std::fs;
 
 pub fn parse(filePath: &str) -> Vec<u8> {
-    let contents = fs::read_to_string(filePath).expect("unable to read file");
+    let fileContent = fs::read_to_string(filePath).expect("unable to read file");
 
-    let mut puzzle: Vec<u8> = Vec::with_capacity(81);
-    parseLines(&mut puzzle, contents);
-    return puzzle;
+    let mut parser = InternalParser::new();
+    parser.parse(fileContent);
+    return parser.puzzle;
 }
 
-fn parseLines(mut puzzle: &mut Vec<u8>, contents: String) {
-    contents.lines()
-        .for_each(|line| parseLine(&mut puzzle, line));
+struct InternalParser {
+    puzzle: Vec<u8>,
 }
 
-fn parseLine(puzzle: &mut Vec<u8>, line: &str) {
-    line.split(',')
-        .for_each(|value| puzzle.push(parseValue(value)))
-}
+impl InternalParser {
+    fn new() -> InternalParser {
+        InternalParser {
+            puzzle: Vec::with_capacity(81),
+        }
+    }
 
-fn parseValue(value: &str) -> u8 {
-    value.trim().parse::<u8>().unwrap()
+    fn parse(&mut self, content: String) {
+        content.lines()
+            .for_each(|line| self.parseLine(line));
+    }
+
+    fn parseLine(&mut self, line: &str) {
+        line.split(',')
+            .for_each(|value| self.puzzle.push(self.parseValue(value)))
+    }
+
+    fn parseValue(&self, value: &str) -> u8 {
+        value.trim().parse::<u8>().unwrap()
+    }
 }
